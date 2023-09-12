75bf15d09a55179337a42d5366088747481ad1a1
tubaamir <135152116+tubaamir@users.noreply.github.com>
2023-09-12 19:20:04 +0530

    Update FactorialUsingRecursion.java

--- FactorialUsingRecursion.java
+++ FactorialUsingRecursion.java
@@ -1,16 +1,21 @@
+//This program finds the factorial of a number
+
 import java.util.Scanner;
 
 public class FactorialUsingRecursion {
     public static void factorialOfNumber(int n) {
         
+        //Base case
         if (n < 0) {
             System.out.println("Invalid Input!");
             return;
         }
+        
         int factorial = 1;
         for (int i=n; i>=1; i--) {
             factorial = factorial * i;
         }
+        
         System.out.print("Factorial of a number is ");
         System.out.println(factorial);
         return;
@@ -18,8 +23,10 @@ public class FactorialUsingRecursion {
 
     public static void main(String []args) {
         Scanner sc = new Scanner(System.in);
+        //input number 
         int num = sc.nextInt();
         
+        //calling function
         factorialOfNumber(num);
         sc.close();
     }
